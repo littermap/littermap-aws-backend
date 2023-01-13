77a09b473d72072e03b898734a5ffa29bc18b2e7
Daniel Toben <danielaarontoben@gmail.com>
2023-01-12 21:17:39 -0500

    Cleaned up indentations and moved done

--- functions/daniel/index.js
+++ functions/daniel/index.js
@@ -9,36 +9,36 @@ const mediaBucket = process.env.MEDIA_BUCKET
 
 exports.handler = async (event, context) => {
     let state = {}
-// get the authentication key from the request headers
+    // get the authentication key from the request headers
     const authKey = event.headers.authentication;
 
 // check if the authKey matches the expected key
     try{
         (authKey !== 'MCEtUbuGM4e9tfMz')
     } catch(e) {
-// if not send a 403 response
-    state.status = 403
-    state.res = error("Unauthorized")
-  }
+      // if not send a 403 response
+      state.status = 403
+      state.res = error("Unauthorized")
+    }
 
 // get the data from the request body
-    const { lat, lng } = JSON.parse(event.body);
-    const data = { lat, lng };
+const { lat, lng } = JSON.parse(event.body);
+const data = { lat, lng };
 
-if (!state.status) {
-    try {
-        async(){
-            s3.putObject( {
-                Bucket: mediaBucket,
-                Key: 'media/daniel.json',
-                Body: JSON.stringify(data)
-
-            } )
+    if (!state.status) {
+        try {
+            async(){
+                s3.putObject( {
+                    Bucket: mediaBucket,
+                    Key: 'media/daniel.json',
+                    Body: JSON.stringify(data)
+                } )
+            }
+        } catch(e) {
+          // if not send a 422 response
+          state.status = 422
+          state.res = error("failed save to bucket")
         }
-    } catch(e) {
-// if not send a 422 response
-    state.status = 422
-    state.res = error("failed save to bucket")
     }
     return done(state, 201)
-}};
+};
