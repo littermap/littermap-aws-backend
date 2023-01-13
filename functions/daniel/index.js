f25cbd77371a1e7f06a1d7b8a9e1c348b4fa871d
Daniel Toben <danielaarontoben@gmail.com>
2023-01-12 20:53:01 -0500

    Applied state, added catches, changed to async

--- functions/daniel/index.js
+++ functions/daniel/index.js
@@ -1,33 +1,44 @@
 
 const s3 = new (require('aws-sdk/clients/s3'))()
+
 const { done } = require('/opt/nodejs/lib/endpoint')
+const { logEvent } = require('/opt/nodejs/lib/interface/eventlog')
+const { error, errorStr } = require('/opt/nodejs/lib/error')
 
 const mediaBucket = process.env.MEDIA_BUCKET
 
-exports.handler = async (event, context, callback) => {
-    // get the authentication key from the request headers
+exports.handler = async (event, context) => {
+    let state = {}
+// get the authentication key from the request headers
     const authKey = event.headers.authentication;
 
-    // check if the authKey matches the expected key
-    if(authKey !== 'MCEtUbuGM4e9tfMz'){
-        // if not send a 403 response
-        callback(null, {
-            statusCode: 403,
-            body: 'Unauthorized'
-        });
-        return;
-    }
+// check if the authKey matches the expected key
+    try{
+        (authKey !== 'MCEtUbuGM4e9tfMz')
+    } catch(e) {
+// if not send a 403 response
+    state.status = 403
+    state.res = error("Unauthorized")
+  }
 
-    // get the data from the request body
+// get the data from the request body
     const { lat, lng } = JSON.parse(event.body);
     const data = { lat, lng };
 
+if (!state.status) {
     try {
-            await s3.putObject( {
-              Bucket: mediaBucket,
-              Key: 'media/daniel.json',
-              Body: JSON.stringify(data)
-            } ).promise()
-          } catch(e) {}
-    return done(201)
-};
+        async(){
+            s3.putObject( {
+                Bucket: mediaBucket,
+                Key: 'media/daniel.json',
+                Body: JSON.stringify(data)
+
+            } )
+        }
+    } catch(e) {
+// if not send a 422 response
+    state.status = 422
+    state.res = error("failed save to bucket")
+    }
+    return done(state, 201)
+}};
